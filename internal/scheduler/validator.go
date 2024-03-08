45cfc33db6f961a21604410765ee81c28cea918e
rhyrak <sgencay88@gmail.com>
2024-03-08 21:09:24 +0300

    Add classroom collision check

--- internal/scheduler/validator.go
+++ internal/scheduler/validator.go
@@ -11,6 +11,10 @@ import (
 func Validate(courses []*model.Course, schedule *model.Schedule, rooms []*model.Classroom) (bool, string) {
 	var message string
 	var valid bool = true
+	var allAssigned bool = false
+	var hasCourseCollision bool = false
+	var hasClassroomCollision bool = false
+
 	unassignedCount := 0
 	var unassignedCourses []*model.Course
 	for _, c := range courses {
@@ -27,6 +31,7 @@ func Validate(courses []*model.Course, schedule *model.Schedule, rooms []*model.
 			message += fmt.Sprintf("    %s %s %d %s\n", un.Course_Code, un.DepartmentCode, un.Number_of_Students, un.Lecturer)
 		}
 	}
+	allAssigned = unassignedCount == 0
 
 	for _, day := range schedule.Days {
 		for _, slot := range day.Slots {
@@ -35,10 +40,45 @@ func Validate(courses []*model.Course, schedule *model.Schedule, rooms []*model.
 					if contains(c1.ConflictingCourses, c2) {
 						valid = false
 						message += "Conflicting courses placed at the same time\n"
+						hasCourseCollision = true
 					}
 				}
 			}
 		}
+	}
+
+	for _, day := range schedule.Days {
+		for _, slot := range day.Slots {
+			var usedRooms map[string]bool = make(map[string]bool)
+			for _, c := range slot.CourseRefs {
+				if c.Classroom == nil {
+					continue
+				}
+				_, usedBefore := usedRooms[c.Classroom.ID]
+				if usedBefore {
+					schedule.Cost++
+					valid = false
+					message += "- Classroom " + c.Classroom.ID + " assigned multiple times\n"
+					hasClassroomCollision = true
+				}
+			}
+		}
+	}
+
+	if hasClassroomCollision {
+		message = "[FAIL]: Classroom collision check.\n" + message
+	} else {
+		message = "[  OK]: Classroom collision check.\n" + message
+	}
+	if hasCourseCollision {
+		message = "[FAIL]: Course collision check.\n" + message
+	} else {
+		message = "[  OK]: Course collision check.\n" + message
+	}
+	if !allAssigned {
+		message = "[FAIL]: Course has room check.\n" + message
+	} else {
+		message = "[  OK]: Course has room check.\n" + message
 	}
 
 	return valid, message
